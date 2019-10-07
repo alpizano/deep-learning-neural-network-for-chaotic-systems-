a60218af4d81afe9d8b2159c47522a04cbc2ab16
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-10-07 16:12:18 -0500

    added gitignore and added vid2img.py

--- Video_to_image/vid2img.py
+++ Video_to_image/vid2img.py
@@ -1,6 +1,6 @@
 import cv2
 print(cv2.__version__)
-vidcap = cv2.VideoCapture('../../data/european-roulette-220.mp4')
+vidcap = cv2.VideoCapture('GoPro_Test/GoProTest_720p_60fps015.mp4')
 success,image = vidcap.read()
 count = 0
 success = True
@@ -9,8 +9,8 @@ success = True
 while success:
     vidcap.set(1,count)
     success,image = vidcap.read()
-    cv2.imwrite("images/frame%d.jpg" % count, image)     # save frame as JPEG file
-    count += 1000
+    cv2.imwrite("images/015_frame%d.jpg" % count, image)     # save frame as JPEG file
+    count += 60
 
 frame_count =  int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
 print(frame_count)
