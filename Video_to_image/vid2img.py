d2bb5972869ac546a216a280335a3ac7d14223d6
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-10-16 15:42:22 -0500

    splicing new video data for beating roulette

--- Video_to_image/vid2img.py
+++ Video_to_image/vid2img.py
@@ -1,6 +1,6 @@
 import cv2
 print(cv2.__version__)
-vidcap = cv2.VideoCapture('GoPro_Test/GoProTest_720p_60fps015.mp4')
+vidcap = cv2.VideoCapture('Beating_Roulette_Data/GOPR0037.MP4')
 success,image = vidcap.read()
 count = 0
 success = True
@@ -9,8 +9,8 @@ success = True
 while success:
     vidcap.set(1,count)
     success,image = vidcap.read()
-    cv2.imwrite("images/015_frame%d.jpg" % count, image)     # save frame as JPEG file
-    count += 60
+    cv2.imwrite("images/0037_frame%d.jpg" % count, image)     # save frame as JPEG file
+    count += 200
 
 frame_count =  int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
 print(frame_count)
