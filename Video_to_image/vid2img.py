d92a24be5432b7c369852c125ac5921bcecba08b
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-10-19 04:46:04 -0500

    automated video to image script for multiple video sets

--- Video_to_image/vid2img.py
+++ Video_to_image/vid2img.py
@@ -1,16 +1,21 @@
 import cv2
 print(cv2.__version__)
-vidcap = cv2.VideoCapture('Beating_Roulette_Data/GOPR0037.MP4')
-success,image = vidcap.read()
-count = 0
-success = True
 
-
-while success:
-    vidcap.set(1,count)
+for i in range(37,139):
+    if i < 100:
+        file_num = "0" + str(i)
+    else:
+        file_num = str(i)
+    vidcap = cv2.VideoCapture('Beating_Roulette_Data/GOPR0%s.MP4' % file_num)
     success,image = vidcap.read()
-    cv2.imwrite("images/0037_frame%d.jpg" % count, image)     # save frame as JPEG file
-    count += 200
+    count = 0
+    success = True
 
-frame_count =  int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-print(frame_count)
+    while success:
+        vidcap.set(1,count)
+        success,image = vidcap.read()
+        cv2.imwrite("images/%s_frame%d.jpg" % (file_num,count), image)     # save frame as JPEG file
+        count += 32
+
+        frame_count =  int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+        print("frame count is: %d" % frame_count)
